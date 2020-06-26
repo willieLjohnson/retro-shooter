0f9a17743b5cf44ed9da053f483bd052453af578
Willie Johnson <liwa.johnson@gmail.com>
2020-06-26 14:37:28 -0400

    Create health manager and add pass through methods for player

---
+++ characters/HealthManager.gd
@@ -0,0 +1,43 @@
+extends Spatial
+
+signal dead
+signal hurt
+signal healed
+signal health_changed
+signal gibbed
+
+export var max_health = 100
+var current_health = 1
+
+export var gibbed_at = -20
+
+func _ready() -> void:
+	init()
+	
+func init():
+	current_health = max_health
+	emit_signal("health_changed", current_health)
+
+func hurt(damage: int, direction: Vector3, damage_type="normal"):
+	if current_health <= 0:
+		return
+	current_health -= damage
+	if current_health <= gibbed_at:
+		pass #TODO make gibs spawner
+		emit_signal("gibbed")
+	if current_health <= 0:
+		emit_signal("dead")
+		print("dead")
+	else:
+		emit_signal("hurt")
+	emit_signal("health_changed", current_health)
+	print("hurt ", damage, "current_health ", current_health)
+
+func heal(amount: int):
+	if current_health <= 0:
+		return
+	current_health += amount
+	if current_health > max_health:
+		current_health = max_health
+	emit_signal("healed")
+	emit_signal("health_changed", current_health)
--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -3,15 +3,23 @@ extends KinematicBody
 export(float) var mouse_sensitivity = 0.5
 onready var character_mover = $CharacterMover
 onready var camera = $Camera
+onready var health_manager = $HealthManager
+
+var dead = false
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	character_mover.init(self)
+	health_manager.init()
+	health_manager.connect("dead", self, "kill")
 	
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
 	
+	if dead:
+		return
+		
 	var move_vector = Vector3()
 	if Input.is_action_pressed("move_forwards"):
 		move_vector += Vector3.FORWARD
@@ -30,3 +38,13 @@ func _input(event: InputEvent) -> void:
 		rotation_degrees.y -= mouse_sensitivity * event.relative.x
 		camera.rotation_degrees.x -= mouse_sensitivity * event.relative.y
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
+
+func hurt(damage, direction):
+	health_manager.hurt(damage, direction)
+
+func heal(amount):
+	health_manager.heal(amount)
+
+func kill():
+	dead = true
+	character_mover.freeze()
