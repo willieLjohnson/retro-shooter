cca61b9c340d30abd36c6e856e35906ec2ad4057
Willie Johnson <liwa.johnson@gmail.com>
2020-06-26 15:39:23 -0400

    Implement weapon switching in player

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -1,9 +1,23 @@
 extends KinematicBody
 
+var hotkeys = {
+	KEY_1: 0,
+	KEY_2: 1,
+	KEY_3: 2,
+	KEY_4: 3,
+	KEY_5: 4,
+	KEY_6: 5,
+	KEY_7: 6,
+	KEY_8: 7,
+	KEY_9: 8,
+	KEY_0: 9,
+}
+
 export(float) var mouse_sensitivity = 0.5
 onready var character_mover = $CharacterMover
 onready var camera = $Camera
 onready var health_manager = $HealthManager
+onready var weapon_manager = $Camera/WeaponManager 
 
 var dead = false
 
@@ -38,7 +52,15 @@ func _input(event: InputEvent) -> void:
 		rotation_degrees.y -= mouse_sensitivity * event.relative.x
 		camera.rotation_degrees.x -= mouse_sensitivity * event.relative.y
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
-
+	if event is InputEventKey and event.pressed:
+		if event.scancode in hotkeys:
+			weapon_manager.switch_to_weapon_slot(hotkeys[event.scancode])
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_WHEEL_DOWN:
+			weapon_manager.switch_to_next_weapon()
+		if event.button_index == BUTTON_WHEEL_UP:
+			weapon_manager.switch_to_last_weapon()
+			
 func hurt(damage, direction):
 	health_manager.hurt(damage, direction)
 
